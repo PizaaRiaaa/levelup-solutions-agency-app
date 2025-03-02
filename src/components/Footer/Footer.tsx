0c2f71ac2d99d1c69e53fd242ebd4139b90ac5f6
bladerunner0001 <bladerunner.pizzaa@gmail.com>
2025-03-03 00:15:36 +0800

    feat: adjusted footer width

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,10 +5,10 @@ export const Footer = () => {
         <div className="overflow-x-hidden max-w-screen font-poppins">
             {/* footer top section */}
             <div className="sm:flex justify-between py-10 px-8 grid bg-gray-900">
-                <div className="text-white w-full">
+                <div className="text-white w-1/2">
                     <h1 className="w-full text-3xl font-bold cursor-pointer text-blue-400">LevelUP Solutions</h1>
                     <p className="py-4">Welcome to LevelUp Solutions, where we transform raw information into actionable insights. With cutting-edge technology and a team of skilled analysts, we specialize in deciphering complex data sets to uncover valuable patterns and trends with which wee take your business to the Next Level.</p>
-                    <div className="flex md:w-[75%] justify-between my-6 text-blue-200">
+                    <div className="flex md:w-[75%] gap-2 justify-between my-6 text-blue-200">
                         {/* social media icons */}
                         <FaFacebookSquare size={30} className="cursor-pointer" />
                         <FaDribbbleSquare size={30} className="cursor-pointer" />
@@ -18,8 +18,8 @@ export const Footer = () => {
                     </div>
                 </div>
                 <div className="text-white w-full sm:w-[900px] sm:ml-[18%] sm:mr-[12%] sm:pt-0">
-                    <div>
-                        <h1>Want tips & Tricks to optimize your Work flow?</h1>
+                    <div className="flex flex-col gap-2 text-center">
+                        <p className="text-xl sm:text-3xl text-center">Want tips & Tricks to optimize your Work flow?</p>
                         <p>Sign up to our newsletter and stay up to date.</p>
                     </div>
                     <div className="my-4">
@@ -28,7 +28,7 @@ export const Footer = () => {
                             <button className="bg-blue-400 mx-auto text-sm w-1/2 hover:cursor-pointer rounded-md hover:bg-blue-500">Sign up</button>
                         </div>
                     </div>
-                    <p className="text-xs mx-10 flex-col text-center pt-4">Wee care about the protection of your data. Read our Privacy <span className="underline cursor-pointer text-blue-400">Policy</span>.</p>
+                    <p className="text-xs mx-10 flex-col text-center pt-4">Wee care about the protection of your data. Read our <span className="underline cursor-pointer text-blue-400">Privacy Policy</span>.</p>
                 </div>
             </div>
             {/* footer top section end */}
